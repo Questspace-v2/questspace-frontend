b867acf8f31e7f6883fd71493c8a0f516218078b
prikotletka <iwantanexcellentmark@gmail.com>
2023-10-29 01:08:14 +0500

    ну и хуета

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        path: './public/favicon.ico',
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- theme/themeConfig.tsx
+++ theme/themeConfig.tsx
@@ -1,11 +1,29 @@
 // theme/themeConfig.ts
 import type { ThemeConfig } from 'antd';
+import { Manrope, Roboto_Flex } from 'next/font/google';
+
+export const manrope = Manrope({
+    style: 'normal',
+    subsets: ['latin', 'cyrillic', 'cyrillic-ext'],
+    weight: ['400', '500', '700'],
+});
+
+const robotoFlex = Roboto_Flex({
+    style: 'normal',
+    subsets: ['latin', 'cyrillic', 'cyrillic-ext'],
+    weight: ['700'],
+});
 
 const theme: ThemeConfig = {
-  // token: {
-  //     fontSize: 16,
-  //     colorPrimary: '#52c41a',
-  // },
+    token: {
+        fontFamily: manrope.style.fontFamily,
+        fontSize: 14,
+    },
+    components: {
+        Typography: {
+            fontFamily: robotoFlex.style.fontFamily,
+        },
+    },
 };
 
 export default theme;
