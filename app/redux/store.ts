de19c225f626ac6f11f44bd60014ab1fc90c824f
RedSquid2212 <Angelina.Shmantsar@urfu.me>
2023-11-20 19:32:30 +0500

    Global state (beginning)

---
+++ app/redux/store.ts
@@ -0,0 +1,10 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+
+export const store = configureStore({
+    reducer: {},
+    devTools: process.env.NODE_ENV !== 'production'
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
