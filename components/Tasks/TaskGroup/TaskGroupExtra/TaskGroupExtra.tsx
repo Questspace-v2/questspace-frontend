30b890ee801d907da87c1cf5730def05583d6afe
Angelina <Angelina.Shmantsar@urfu.me>
2024-05-07 16:05:24 +0500

    Редактирование названия раздела

--- components/Tasks/TaskGroup/TaskGroupExtra/TaskGroupExtra.tsx
+++ components/Tasks/TaskGroup/TaskGroupExtra/TaskGroupExtra.tsx
@@ -5,16 +5,19 @@ import {Button, ConfigProvider, Dropdown, Form, MenuProps, UploadFile} from 'ant
 import { blueOutlinedButton, redOutlinedButton } from '@/lib/theme/themeConfig';
 import { DeleteOutlined, EditOutlined, MenuOutlined, PlusOutlined } from '@ant-design/icons';
 import dynamic from 'next/dynamic';
-import {TaskForm} from "@/components/Tasks/Task/EditTask/EditTask";
-import {useTasksContext} from "@/components/Tasks/ContextProvider/ContextProvider";
+import {TaskForm} from '@/components/Tasks/Task/EditTask/EditTask';
+import {useTasksContext} from '@/components/Tasks/ContextProvider/ContextProvider';
 
 const DynamicEditTask = dynamic(() => import('@/components/Tasks/Task/EditTask/EditTask'),
-    {ssr: false})
+    {ssr: false});
+const DynamicEditTaskGroup = dynamic(() => import('@/components/Tasks/TaskGroup/EditTaskGroup/EditTaskGroup'),
+    {ssr: false});
 
 export default function TaskGroupExtra({edit, taskGroupName}: {edit: boolean, taskGroupName: string}) {
     const [open, setOpen] = useState(false);
     const [isOpenCreateModal, setIsOpenCreateModal] = useState(false);
     const [fileList, setFileList] = useState<UploadFile[]>([]);
+    const [isOpenNameModal, setIsOpenNameModal] = useState(false);
     const [form] = Form.useForm<TaskForm>();
 
     const {data: contextData, updater: setContextData} = useTasksContext()!;
@@ -25,6 +28,10 @@ export default function TaskGroupExtra({edit, taskGroupName}: {edit: boolean, ta
 
     const handleOpenChange = (flag: boolean) => {
         setOpen(flag);
+    };
+
+    const handleChangeName = () => {
+        setIsOpenNameModal(true);
     };
 
     const handleAddTask = () => {
@@ -39,6 +46,7 @@ export default function TaskGroupExtra({edit, taskGroupName}: {edit: boolean, ta
         {
             label: <><EditOutlined/>Изменить название</>,
             key: '1',
+            onClick: handleChangeName,
         },
         {
 
@@ -48,8 +56,9 @@ export default function TaskGroupExtra({edit, taskGroupName}: {edit: boolean, ta
         },
         {
 
-            label: <><DeleteOutlined onClick={handleDeleteGroup}/>Удалить раздел</>,
+            label: <><DeleteOutlined/>Удалить раздел</>,
             key: '3',
+            onClick: handleDeleteGroup
         },
     ];
 
@@ -57,7 +66,7 @@ export default function TaskGroupExtra({edit, taskGroupName}: {edit: boolean, ta
         return (
             <div className={'task-group__collapse-buttons'}>
                 <ConfigProvider theme={blueOutlinedButton}>
-                    <Button><EditOutlined/>Изменить название</Button>
+                    <Button onClick={handleChangeName}><EditOutlined/>Изменить название</Button>
                     <Button onClick={handleAddTask}><PlusOutlined/>Добавить задачу</Button>
                 </ConfigProvider>
                 <ConfigProvider theme={redOutlinedButton}>
@@ -84,6 +93,11 @@ export default function TaskGroupExtra({edit, taskGroupName}: {edit: boolean, ta
                     fileList={fileList}
                     setFileList={setFileList}
                     form={form}
+                />
+                <DynamicEditTaskGroup
+                    taskGroupName={taskGroupName}
+                    isOpen={isOpenNameModal}
+                    setIsOpen={setIsOpenNameModal}
                 />
             </div>
         );
