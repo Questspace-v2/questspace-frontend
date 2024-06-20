f259228dbecc85f9a07a7f23c07958251532b618
cutletmedie <cutletmedie@gmail.com>
2024-06-20 21:18:26 +0500

    Новые иконочки

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -29,10 +29,10 @@ export default async function RootLayout({ children }: React.PropsWithChildren) 
     return (
         <html lang="ru">
         <head>
-            <link rel="apple-touch-icon" sizes="180x180" href="../icons/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="../icons/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="../icons/favicon-16x16.png" />
-            <link rel="mask-icon" href="../icons/safari-pinned-tab.svg" color="#5bbad5" />
+            <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png" />
+            <link rel="mask-icon" href="../safari-pinned-tab.svg" color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#428df4" />
             <meta name="theme-color" content="#ffffff" />
         </head>
