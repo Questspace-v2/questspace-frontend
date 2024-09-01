2604fa4240ba5069cf38cd23c5717dbf919aecf8
Yulia <cutletmedie@gmail.com>
2024-09-01 17:44:50 +0500

    Metadata (#85)
    * Новая основная метадата
    
    * robots.txt и sitemap.xml

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: '*',
-            allow: ['/', '/auth'],
+            allow: ['/', '/auth', '/quest/'],
             disallow: []
         },
         sitemap: `${FRONTEND_URL}/sitemap.xml`
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,36 @@
 import { FRONTEND_URL } from '@/app/api/client/constants';
+import getBackendQuests from '@/components/QuestTabs/QuestTabs.server';
+import { IQuest } from '@/app/types/quest-interfaces';
 
 
-export default function sitemap() {
+export default async function sitemap() {
+    let nextPageId: string | undefined = '';
+    const questIds: string[] = [];
+    while (nextPageId !== undefined) {
+        // eslint-disable-next-line no-await-in-loop
+        const data = await getBackendQuests('all', nextPageId, '50');
+        questIds.push(...(data?.quests ?? []).map((quest: IQuest) => quest.id) ?? []);
+
+        nextPageId = data?.next_page_id;
+    }
+
+    const questsSitemap = questIds.map((id: string) => ({
+        url:`${FRONTEND_URL}/quest/${id}`,
+        lastModified: new Date(),
+        priority: 0.5
+    }))
+
     return [
         {
             url: FRONTEND_URL,
             lastModified: new Date(),
+            priority: 1
         },
         {
             url: `${FRONTEND_URL}/auth`,
             lastModified: new Date(),
+            priority: 0.8
         },
+        ...questsSitemap
     ]
 }
