13c163171baba61bf5c27219c49f62283b2fa973
Shmantsar Angelina <92442238+RedSquid2212@users.noreply.github.com>
2024-09-21 23:41:16 +0500

    Green title with score (#101)
    * Отображаем принятый ответ после обновления страницы
    
    * Навалили стилей и зеленую шапку для группы
    
    * Расшарили контекст на плей-мод, пофиксили менеджмент зеленого текста
    
    * Пофиксили класснеймы
    
    * Правки верстки и микробагов рядом
    
    ---------
    
    Co-authored-by: cutletmedie <cutletmedie@gmail.com>

--- components/Tasks/Task/Task.tsx
+++ components/Tasks/Task/Task.tsx
@@ -1,19 +1,21 @@
 'use client';
 
 import Image from 'next/image';
-import { uid } from '@/lib/utils/utils';
-import { Button, CountdownProps, Form, Input, message, Statistic } from 'antd';
-import { IHintRequest, ITask, ITaskAnswer, ITaskAnswerResponse, ITaskGroup } from '@/app/types/quest-interfaces';
-import { SendOutlined } from '@ant-design/icons';
+import {uid} from '@/lib/utils/utils';
+import {Button, CountdownProps, Form, Input, message, Statistic} from 'antd';
+import {IHintRequest, ITask, ITaskAnswer, ITaskAnswerResponse, ITaskGroup} from '@/app/types/quest-interfaces';
+import {SendOutlined} from '@ant-design/icons';
 import FormItem from 'antd/lib/form/FormItem';
-import { getTaskExtra, TasksMode } from '@/components/Tasks/Tasks.helpers';
-import { answerTaskPlayMode, takeHintPlayMode } from '@/app/api/api';
-import { useSession } from 'next-auth/react';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import {getTaskExtra, TasksMode} from '@/components/Tasks/Tasks.helpers';
+import {answerTaskPlayMode, takeHintPlayMode} from '@/app/api/api';
+import {useSession} from 'next-auth/react';
+import {useState} from 'react';
+import {useRouter} from 'next/navigation';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import classNames from 'classnames';
+import {useTasksContext} from '@/components/Tasks/ContextProvider/ContextProvider';
+import {RELEASED_FEATURE} from '@/app/api/client/constants';
 
 const { Countdown } = Statistic;
 const enum SendButtonStates {
@@ -37,7 +39,18 @@ interface TaskProps {
 }
 
 export default function Task({mode, props, questId, taskGroupProps}: TaskProps) {
-    const {name, question, hints, media_link: mediaLink, correct_answers: correctAnswers, id: taskId, answer: teamAnswer} = props;
+    const {
+        name,
+        question,
+        hints,
+        media_link: mediaLink,
+        correct_answers: correctAnswers,
+        id: taskId,
+        answer: teamAnswer,
+        score,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        reward
+    } = props;
     const [openConfirm, setOpenConfirm] = useState(false);
     const [openConfirmIndex, setOpenConfirmIndex] = useState<0 | 1 | 2 | null>(null);
     const [takenHints, setTakenHints] = useState([false, false, false]);
@@ -55,10 +68,15 @@ export default function Task({mode, props, questId, taskGroupProps}: TaskProps) 
     const [messageApi, contextHolder] = message.useMessage();
     const router = useRouter();
 
+    const { updater: setContextData} = useTasksContext()!;
+    const currentTaskGroupId = taskGroupProps.id;
+
     const [sendButtonState, setSendButtonState] = useState<SendButtonStates>(teamAnswer ? SendButtonStates.DISABLED : SendButtonStates.BASIC);
     const [inputState, setInputState] = useState<InputStates>(teamAnswer ? InputStates.ACCEPTED : InputStates.BASIC);
     const [sendButtonContent, setSendButtonContent] = useState<JSX.Element | null>(<SendOutlined/>);
     const [inputValidationStatus, setInputValidationStatus] = useState<'success' | 'error' | ''>(teamAnswer ? 'success' : '');
+
+    const [accepted, setAccepted] = useState(Boolean(score && score > 0));
 
     const onFinish: CountdownProps['onFinish'] = () => {
         setSendButtonContent(<SendOutlined/>);
@@ -108,6 +126,7 @@ export default function Task({mode, props, questId, taskGroupProps}: TaskProps) 
         setInputState(InputStates.ACCEPTED);
         setSendButtonContent(<SendOutlined/>);
         setSendButtonState(SendButtonStates.DISABLED);
+        setAccepted(true);
         success();
     };
 
@@ -122,6 +141,24 @@ export default function Task({mode, props, questId, taskGroupProps}: TaskProps) 
         if (answerResponse.accepted) {
             setInputValidationStatus('success');
             form.setFieldValue('task-answer', answerResponse.text);
+            setContextData(prevState => {
+                if (currentTaskGroupId && RELEASED_FEATURE) {
+                    const taskGroups = prevState.task_groups;
+                    const taskGroup = taskGroups
+                        .find(item => item.id === currentTaskGroupId)!;
+                    const taskGroupIndex = taskGroups.indexOf(taskGroup);
+                    const currentTaskIndex = taskGroup.tasks.indexOf(props);
+                    taskGroup.tasks[currentTaskIndex] = {
+                        ...props,
+                        score: answerResponse.score,
+                    };
+                    taskGroups[taskGroupIndex] = taskGroup;
+                    return {
+                        task_groups: taskGroups,
+                    };
+                }
+                return prevState;
+            })
             handleAccept();
         } else {
             setInputValidationStatus('error');
@@ -148,7 +185,14 @@ export default function Task({mode, props, questId, taskGroupProps}: TaskProps) 
         <div className={'task__wrapper'}>
             {contextHolder}
             <div className={'task__text-part'}>
-                <h4 className={'roboto-flex-header task__name'}>{name}</h4>
+                {
+                    RELEASED_FEATURE && mode === TasksMode.PLAY ?
+                        <h4 className={classNames('roboto-flex-header task__name', accepted && 'task__accepted')}>
+                            {name}
+                            {/* {accepted && <span>(+{reward})</span>} */}
+                        </h4> :
+                        <h4 className={'roboto-flex-header task__name'}>{name}</h4>
+                }
                 {getTaskExtra(mode === TasksMode.EDIT, true, taskGroupProps, props, questId)}
                 <Markdown className={'task__question line-break'} disallowedElements={['pre', 'code']} remarkPlugins={[remarkGfm]}>{question}</Markdown>
             </div>
@@ -193,16 +237,14 @@ export default function Task({mode, props, questId, taskGroupProps}: TaskProps) 
                 className={'task__answer-part'}
                 layout={'inline'}
                 form={form}
-                initialValues={[
-                    {name: 'task-answer', value: teamAnswer ?? ''}
-                ]}
+                initialValues={{'task-answer': teamAnswer ?? ''}}
             >
                 <Form.Item required
                            name={'task-answer'}
                            validateStatus={inputValidationStatus}
                            hasFeedback>
                     <Input
-                        className={classNames(InputStates.ACCEPTED && 'task__answer-part_right')}
+                        className={classNames(inputState === InputStates.ACCEPTED && 'task__answer-part_right')}
                         placeholder={'Ответ'}
                         onChange={handleValueChange}
                         disabled={inputState === InputStates.ACCEPTED} onPressEnter={handleSendAnswer}/>
