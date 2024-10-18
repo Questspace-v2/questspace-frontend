68feb92e15dc42046912252dac34893156c1376b
Shmantsar Angelina <Angelina.Shmantsar@urfu.me>
2024-10-18 21:16:48 +0500

    Накидали стилей на таблицу

--- components/QuestAdmin/Logs/Logs.tsx
+++ components/QuestAdmin/Logs/Logs.tsx
@@ -1,13 +1,15 @@
 import { IAnswerLog, IPaginatedAnswerLogs } from '@/app/types/quest-interfaces';
 import { Table } from 'antd';
 import { TableProps } from 'antd/lib';
+import classNames from 'classnames';
 
 export default function Logs({paginatedAnswerLogs}: {paginatedAnswerLogs: IPaginatedAnswerLogs}) {
     const columns: TableProps<IAnswerLog>['columns'] = [
         {
             title: 'Время',
             dataIndex: 'answer_time',
-            key: 'answer_time'
+            key: 'answer_time',
+            render: (time: string) => new Date(time).toLocaleString().replace(',', ''),
         },
         {
             title: 'Группа',
@@ -32,13 +34,21 @@ export default function Logs({paginatedAnswerLogs}: {paginatedAnswerLogs: IPagin
         {
             title: 'Ответ',
             dataIndex: 'answer',
-            key: 'answer'
+            key: 'answer',
+            render: (_, record) =>
+                <span className={classNames('logs-table__answer', record.accepted ? 'accepted' : 'rejected')}>{record.answer}</span>
         }
     ];
 
     const dataSource = paginatedAnswerLogs.answer_logs;
 
     return (
-        <Table<IAnswerLog> columns={columns} dataSource={dataSource}/>
+        <Table<IAnswerLog> 
+            columns={columns} 
+            dataSource={dataSource} 
+            pagination={false}
+            rowKey={(log) => log.answer_time}
+            className='logs-table__table'
+        />
     );
 }
