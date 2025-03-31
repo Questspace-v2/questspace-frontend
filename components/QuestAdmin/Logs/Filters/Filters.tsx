54bdc1bc4216715728b44e82d8ee44eee975de8b
Yulia <cutletmedie@gmail.com>
2025-04-01 04:39:33 +0500

    Поиск в фильтрах логов

--- components/QuestAdmin/Logs/Filters/Filters.tsx
+++ components/QuestAdmin/Logs/Filters/Filters.tsx
@@ -41,6 +41,12 @@ export default function Filters({ options, setSelectedFilters }: FiltersProps) {
             <Select
                 placeholder='Группа'
                 allowClear
+                showSearch
+                autoClearSearchValue
+                optionFilterProp={'label'}
+                filterSort={(optionA, optionB) =>
+                    (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+                }
                 options={options?.groups}
                 className='answer-logs__filter'
                 onChange={(value: string) => onFilterChange('group', value)}
@@ -48,6 +54,12 @@ export default function Filters({ options, setSelectedFilters }: FiltersProps) {
             <Select
                 placeholder='Задача'
                 allowClear
+                showSearch
+                autoClearSearchValue
+                optionFilterProp={'label'}
+                filterSort={(optionA, optionB) =>
+                    (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+                }
                 options={options?.tasks}
                 className='answer-logs__filter'
                 onChange={(value: string) => onFilterChange('task', value)}
@@ -55,6 +67,12 @@ export default function Filters({ options, setSelectedFilters }: FiltersProps) {
             <Select
                 placeholder='Команда'
                 allowClear
+                showSearch
+                autoClearSearchValue
+                optionFilterProp={'label'}
+                filterSort={(optionA, optionB) =>
+                    (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+                }
                 options={options?.teams}
                 className='answer-logs__filter'
                 onChange={(value: string) => onFilterChange('team', value)}
@@ -62,6 +80,12 @@ export default function Filters({ options, setSelectedFilters }: FiltersProps) {
             <Select
                 placeholder='Пользователь'
                 allowClear
+                showSearch
+                autoClearSearchValue
+                optionFilterProp={'label'}
+                filterSort={(optionA, optionB) =>
+                    (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+                }
                 options={options?.users}
                 className='answer-logs__filter'
                 onChange={(value: string) => onFilterChange('user', value)}
