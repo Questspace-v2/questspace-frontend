0268c3bf8f52d73f36feec5f902504ef00187180
Yulia <cutletmedie@gmail.com>
2025-04-12 15:59:50 +0500

    Фикс сессии через повторный пинг (#152)

--- components/NextAuthProvider/NextAuthProvider.tsx
+++ components/NextAuthProvider/NextAuthProvider.tsx
@@ -3,6 +3,7 @@
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
 import React, { ReactNode } from 'react';
+import SessionRefetchEvents from '@/components/NextAuthProvider/SessionRefetchEvents';
 
 export default function NextAuthProvider({children, session}: {children: ReactNode, session: Session | null}) {
     return (
@@ -11,6 +12,7 @@ export default function NextAuthProvider({children, session}: {children: ReactNo
             refetchInterval={10 * 60}
             refetchOnWindowFocus
         >
+            <SessionRefetchEvents />
             {children}
         </SessionProvider>
     );
